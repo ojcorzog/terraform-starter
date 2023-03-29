dcf4ba840036ee479cae546b1d208d137e88f31b
oscar.corzo <ojcorzog@gmail.com>
2023-03-28 20:35:24 -0500

    length 38

--- context.tf
+++ context.tf
@@ -28,7 +28,7 @@ resource "spacelift_environment_variable" "context-plaintext" {
 # For another (secret) variable, let's create programmatically create a super
 # secret password.
 resource "random_password" "context-password" {
-  length  = 36
+  length  = 38
   special = true
 }
 
